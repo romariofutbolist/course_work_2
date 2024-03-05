993d67dfd26d5f3e478e39dfcb5d2efa39087dea
romariofutbolist <66all@mail.ru>
2024-03-05 21:41:53 +0300

    Проверка работы программы перед началом выполнения проекта

---
+++ src/main/java/prosky/course_work_2/CourseWork2Application.java
@@ -0,0 +1,13 @@
+package prosky.course_work_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork2Application.class, args);
+	}
+
+}
---
+++ src/test/java/prosky/course_work_2/CourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package prosky.course_work_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
